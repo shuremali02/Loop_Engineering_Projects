85488f5e124accbb6fef31b3cbb1d7794ca7558a
shuremali02 <shuremsyed41@gmail.com>
2026-08-17 01:10:31 +0500

    project-6: fix planted None-guard bugs (tests synchronize event re-fire)

--- project-6-doorbell-loop/sample_repo/shipping.py
+++ project-6-doorbell-loop/sample_repo/shipping.py
@@ -3,8 +3,12 @@
 
 def calculate_shipping_cost(weight, discount=None):
     base_cost = weight * 5
+    if discount is None:
+        discount = 0
     return base_cost - discount
 
 
 def apply_bulk_rate(item_count, rate=None):
+    if rate is None:
+        rate = 1
     return item_count * rate
